d8bd8f83ebfb127746842758c106cc493d43f044
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-02-25 17:49:28 +0900

    ルーティングの誤りを修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "statatic_pages#index"
+  root "sta tic_pages#index"
 end
