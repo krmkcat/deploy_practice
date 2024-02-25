c02bf3a46e269741e905bb2e9bc4fd3708f27d50
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-02-25 18:14:16 +0900

    entrypoint.shにCSSおよびjS

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,15 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+# Build CSS and JavaScript
+bundle exec rails css:build RAILS_ENV=production
+bundle exec rails js:build RAILS_ENV=production
+
+# Precompile assets
 bundle exec rails assets:precompile RAILS_ENV=production
 bundle exec rails assets:clean RAILS_ENV=production
+
+# Database migration
 bundle exec rails db:migrate RAILS_ENV=production
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
