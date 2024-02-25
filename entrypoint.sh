878f44b1af63e29b720815fef4c2e8eef569eb38
中山真奈葉 <127219121+krmkcat@users.noreply.github.com>
2024-02-25 17:11:47 +0900

    entrypoint.shを修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,10 @@ set -e
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /app/tmp/pids/server.pid
 
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
